5cb4e1ed74e19ae5507daa3824551951113f69b2
H_Tatsuguchi <h.tatsuguchi.1234@gmail.com>
2016-07-31 08:38:22 +0900

    一時保存

--- app/src/main/java/jp/eq_inc/testapplication/manager/PresetDataLoader.java
+++ app/src/main/java/jp/eq_inc/testapplication/manager/PresetDataLoader.java
@@ -2,6 +2,9 @@ package jp.eq_inc.testapplication.manager;
 
 import android.content.Context;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 public class PresetDataLoader extends DataLoader {
     public PresetDataLoader(Context context) {
         super(context);
@@ -9,6 +12,13 @@ public class PresetDataLoader extends DataLoader {
 
     @Override
     protected boolean loadData() {
+        InputStream contentListInputStream = null;
+        try {
+            contentListInputStream = mContext.getAssets().open("content_list.json");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return false;
     }
 }
